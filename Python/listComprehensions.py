35e0143513afda37bee48069b1cc3d04a19caefb
dantevil <balavignesh25@gmail.com>
2018-12-09 00:10:59 +0530

    list comprehension

--- Python/listComprehensions.py
+++ Python/listComprehensions.py
@@ -6,7 +6,6 @@ Type "help", "copyright", "credits" or "license()" for more information.
     z = int(input())
     n = int(input())
     arr = [[X, Y, Z] for X in range(x+1) for Y in range(y+1) for Z in range(z+1) if X + Y + Z != n]
-#Output
 print(arr)
 SyntaxError: invalid syntax
 >>> 
