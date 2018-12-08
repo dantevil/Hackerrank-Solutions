5effb47b382f3e990e94ff4cf02840d9a29cfc05
dantevil <balavignesh25@gmail.com>
2018-12-09 00:12:00 +0530

    Update listComprehensions.py

--- Python/listComprehensions.py
+++ Python/listComprehensions.py
@@ -1,11 +1,9 @@
-Python 3.6.7 (v3.6.7:6ec5cf24b7, Oct 20 2018, 13:35:33) [MSC v.1900 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> if __name__ == '__main__':
+
+ if __name__ == '__main__':
     x = int(input())
     y = int(input())
     z = int(input())
     n = int(input())
     arr = [[X, Y, Z] for X in range(x+1) for Y in range(y+1) for Z in range(z+1) if X + Y + Z != n]
 print(arr)
-SyntaxError: invalid syntax
->>> 
+
